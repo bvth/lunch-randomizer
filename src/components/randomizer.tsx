2e3fbba0cbb36e392657fd37132fa599db1d0391
thanh.bui1995@gmail.com <thanh.bui1995@gmail.com>
2022-06-19 12:38:28 +0300

    find by location

--- src/components/randomizer.tsx
+++ src/components/randomizer.tsx
@@ -4,30 +4,54 @@ import axios from "axios";
 
 export default function Randomizer () {
     const [restaurantList, setRestaurantList] = useState<any[]>([]);
+    const [responseLimit, setResponseLimit] = useState<number>(100);
+    const [isLoading, setLoading] = useState<boolean>(true);
+    const [chosenOne, setChosenOne] = useState<any>({});
 
     useEffect(() => {
-        axios({
-            ...options,
-            url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng',
-            params: {
-                latitude: '60.1667969',
-                longitude: '24.9452124',
-                limit: '30',
-                distance: '2',
-                open_now: 'false',
-                lunit: 'km',
-                lang: 'en_US'
-            },
-        }).then(
-            res => {
-                setRestaurantList(res.data.data);
-            }
-        ).catch( e => {
+        const success = (position: GeolocationPosition) => {
+            let lat = position.coords.latitude;
+            let lon = position.coords.longitude;
+            axios({
+                ...options,
+                url: 'https://travel-advisor.p.rapidapi.com/restaurants/list-by-latlng',
+                params: {
+                    latitude: lat,
+                    longitude: lon,
+                    limit: responseLimit,
+                    distance: '1',
+                    open_now: 'false',
+                    lunit: 'km',
+                    lang: 'en_US'
+                },
+            }).then(
+                res => {
+                    setResponseLimit(res.data.paging.total_results);
+                    setRestaurantList(res.data.data);
+                }
+            ).catch( e => {
+                console.error(e);
+            }).then(() => {
+                setLoading(false);
+            })
+        }
+
+        const error = (e: GeolocationPositionError) => {
             console.error(e);
-        })
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error);
     },[])
 
-    return restaurantList.length ?
-        <h1>{restaurantList[Math.round(Math.random() * 32)].name}</h1>
-        : <span>nothing, sorry</span>
+    useEffect(() => {
+        if(restaurantList.length)
+            setChosenOne(restaurantList[Math.round(Math.random() * responseLimit)])
+    }, [responseLimit])
+
+    return !isLoading && chosenOne?
+        <>
+            <h1>{chosenOne.name}</h1>
+            <p>Found {responseLimit} restaurants</p>
+        </>
+        : <span>still looking</span>
 }
